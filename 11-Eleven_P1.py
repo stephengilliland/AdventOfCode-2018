8e2af7f45e11da8f80ec5952324ff878c6eb0401
Stephen Gilliland <stephenmatsuta@gmail.com>
2018-12-31 14:29:53 -0500

    Add files via upload

--- 11-Eleven_P1.py
+++ 11-Eleven_P1.py
@@ -1,9 +1,7 @@
 currPower, bestPower, locX, locY, currPoint, d = 0, 0, 0, 0, 0, []
 for y in range(1, 301):
     for x in range(1, 301):
-        currPoint = (((((x+10)*y)+3999)*(x+10)//100)%10) - 5
-        d.append(currPoint)
-#for Y in range(1, 298):
+        d.append((((((x+10)*y)+3999)*(x+10)//100)%10) - 5)
 for x in range(1, 89398):
     currPower = d[x] + d[x+1] + d[x+2] + d[x+300] + d[x+301] + d[x+302] + d[x+600] + d[x+601] + d[x+602]
     if(currPower > bestPower):
